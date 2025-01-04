692ea0d223aff9ff971432eab39322bbb00093bd
Fabri <ism.fabri@gmail.com>
2025-01-04 18:53:28 -0300

    update_styles

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*
 function loadHTML(divID, filepath) {
     fetch(filepath)
         .then(response => response.text())
@@ -20,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+/*
 document.addEventListener('DOMContentLoaded', function() {
     const isGitHubPages = window.location.hostname === 'fab1471.'; // Verify if the website is hosted on GitHub Pages
     const repoName = 'Portfolio' // Repository name. .
@@ -34,50 +34,5 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     };
 });
+
 */
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Função para carregar HTML dinamicamente
-    function loadHTML(divID, filepath) {
-        fetch(filepath)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById(divID).innerHTML = data;
-            })
-            .catch(error => console.error('Error loading HTML:', error));
-    }
-
-    // Ajusta o carregamento do header e footer com base no caminho da página
-    const pagePath = window.location.pathname;
-    if (pagePath.includes('/pages/')) {
-        loadHTML('header', '../components/header.html');
-        loadHTML('footer', '../components/footer.html');
-    } else {
-        loadHTML('header', 'components/header.html');
-        loadHTML('footer', 'components/footer.html');      
-    }
-
-    // Detecta se está no GitHub Pages e ajusta os links
-    const isGitHubPages = window.location.hostname === 'fab1471.github.io'; // Substitua 'username' pelo seu nome de usuário
-    const repoName = 'Portfolio'; // Nome do seu repositório no GitHub
-
-    if (isGitHubPages) {
-        const links = document.querySelectorAll('a');
-        links.forEach(link => {
-            let href = link.getAttribute('href');
-            if (!href.startsWith('/') && !href.includes('http')) {
-                link.setAttribute('href', `/${repoName}/${href}`);
-            }
-        });
-    }
-
-    // Adicionar manipulador para eventos de navegação
-    window.addEventListener('popstate', function() {
-        // Recarregar HTML do header e footer em cada navegação
-        loadHTML('header', 'components/header.html');
-        loadHTML('footer', 'components/footer.html');
-    });
-});
